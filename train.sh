78f0718c091623193c3ac5f17868b1eb0543ec75
kongwu <31300706+shenchous@users.noreply.github.com>
2023-02-10 07:46:37 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python -m torch.distributed.launch --nproc_per_node=1 main.py \
 --model convnext_tiny --drop_path 0.5 --input_size 384 \
---batch_size 2 --lr 2e-4 --update_freq 2 --nb_classes 2 \
+--batch_size 2 --lr 2e-4 --update_freq 2 --nb_classes 3 \
 --warmup_epochs 0 --epochs 60 --weight_decay 1e-8  \
 --head_init_scale 0.001 --cutmix 0 --mixup 0 \
 --save_ckpt_num 100 --dist_eval False \
